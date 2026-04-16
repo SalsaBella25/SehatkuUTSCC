db44e118bc568a97976c845d60d44c1b697632c3
Nazwa Nur Salsa Bella <nazwa.nur@mhs.itenas.ac.id>
2026-04-16 09:41:21 +0700

    Fix bucket name and remove session token

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ const app = express();
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
-    sessionToken: process.env.AWS_SESSION_TOKEN, 
     region: process.env.AWS_REGION || 'us-east-1'
 });
 
